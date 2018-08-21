bddfc27ac7ca90973d5d125ec04f0e887594d731
cardona150 <575891+cardona150@users.noreply.github.com>
2018-08-20 21:47:41 -0700

    Initial

---
+++ Assets/Scripts/MapSpawner.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpawner : MonoBehaviour
+{
+    public GameObject RoadTile;
+    public GameObject GrassTile;
+    public GameObject[] BuildingTiles;
+    public byte NumberOfRows;
+    public byte NumberOfColumns;
+    public byte TileSize;
+    public float TileMoveSpeed;
+    private GameObject[] LastRow;
+    private uint newRowSpawnFixedUpdates;
+    private byte currentFixedUpdates = 0;
+    private System.Random random = new System.Random();
+
+    void Start()
+    {
+        newRowSpawnFixedUpdates = (uint)((float)TileSize / TileMoveSpeed);
+        LastRow = new GameObject[NumberOfColumns];
+        for (int z = 0; z < NumberOfRows; z++)
+        {
+            SpawnRow(z * TileSize);
+        }
+    }
+
+    private void SpawnRow(float z)
+    {
+        for (int x = 0; x < NumberOfColumns; x++)
+        {
+            GameObject tile = null;
+            if (x < (NumberOfColumns / 2) - 1 || x > (NumberOfColumns / 2) + 1)
+            {
+                tile = BuildingTiles[random.Next(0, 2)];
+            }
+            else if (x == (NumberOfColumns / 2) - 1 || x == (NumberOfColumns / 2) + 1)
+            {
+                tile = GrassTile;
+            }
+            else if (x == NumberOfColumns / 2)
+            {
+                tile = RoadTile;
+            }
+
+            var tempTile = Instantiate(tile);
+            var position = new Vector3((x - (NumberOfColumns / 2)) * TileSize, 0, z);
+            tempTile.transform.position = position;
+            tempTile.transform.parent = transform;
+            LastRow[x] = tempTile;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Happens every 0.2 seconds (Edit > Project Settings > Time> Fixed Timestep)
+    void FixedUpdate()
+    {
+        foreach (Transform childTransform in transform)
+        {
+            childTransform.position += new Vector3(0, 0, -TileMoveSpeed);
+        }
+
+        currentFixedUpdates++;
+        if (currentFixedUpdates == newRowSpawnFixedUpdates)
+        {
+            SpawnRow((NumberOfRows - 1) * TileSize);
+            currentFixedUpdates = 0;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("MapElement"))
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float windForceMaximumMagnitude;
+    public float secondsBetweenWindChange = 1;
+
+    private float secondsSinceLastWindChange;
+    private float windForce;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //transform.position = new Vector3(0, 0, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        // Accumulate the amount of seconds since last FixedUpdate
+        secondsSinceLastWindChange += Time.fixedDeltaTime;
+
+        //If it's been long enough, generate a new random wind force
+        if (secondsSinceLastWindChange > secondsBetweenWindChange)
+        {
+            windForce = UnityEngine.Random.Range(-windForceMaximumMagnitude, windForceMaximumMagnitude);
+            //windForce = 0;
+
+            // Reset the accumulator
+            secondsSinceLastWindChange = 0;
+        }
+
+        // This will always be between -1 and 1
+        var horizontalAxisForce = Input.GetAxis("Horizontal");
+
+        // The more tilted the player is the stronger their rebalance should be
+        // Use this help factor to achieve it
+        float boost = 0;
+
+        // In Unity euler angles are
+        //     0/360
+        //       ↑
+        //  90 ←---→ 270
+        //       ↓ 
+        //      180
+        if (0 <= transform.rotation.eulerAngles.z && transform.rotation.eulerAngles.z <= 90)
+        {
+            // Divide the angle by something because a boost of more than 45 is too strong
+            //boost = (transform.rotation.eulerAngles.z) / 5;
+            boost = (transform.rotation.eulerAngles.z) / 5;
+        }
+        else if (270 <= transform.rotation.eulerAngles.z && transform.rotation.eulerAngles.z <= 360)
+        {
+            // Divide the angle by something because a boost of more than 45 is too strong
+            //boost = (360 - transform.rotation.eulerAngles.z) / 5;
+            boost = (360 - transform.rotation.eulerAngles.z) / 5;
+        }
+
+        // Calculate the final vorce vector
+        var finalForceVector3 = new Vector3((horizontalAxisForce * boost) + windForce, 0.0f, 0.0f);
+
+        // Actually add the force
+        rb.AddRelativeForce(finalForceVector3);
+    }
+}
