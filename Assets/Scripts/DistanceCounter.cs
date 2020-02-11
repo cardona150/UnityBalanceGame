4163c8d9e7dfa0826fde9948356bd5b08188e205
cardona150 <575891+cardona150@users.noreply.github.com>
2020-02-10 17:24:24 -0800

    Add stopwatch

---
+++ Assets/Scripts/DistanceCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCounter : MonoBehaviour
+{
+    public Text distance;
+    private Stopwatch stopwatch = new Stopwatch();
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopwatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance.text = stopwatch.Elapsed.TotalSeconds.ToString();
+    }
+}
